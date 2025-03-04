eecb6996c9f37459643fbaac2f581504005caf4d
rushikeshballa <rushikeshballa27@gmail.com>
2025-03-04 11:19:42 +0530

    first commit

---
+++ method.java
@@ -0,0 +1,10 @@
+public class method {
+
+    static void mymethod() {
+        System.out.println("my MEthord");
+    }
+    public static void main(String[] args) {
+        mymethod();
+    }
+    
+}
