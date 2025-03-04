c92f86b22124f8967b997345421b2d4d357badd9
rushikeshballa <rushikeshballa27@gmail.com>
2025-03-04 11:23:54 +0530

    asd

--- method.java
+++ method.java
@@ -3,8 +3,12 @@ public class method {
     static void mymethod() {
         System.out.println("my MEthord");
     }
+    static void function(int x,int y){
+        System.out.println(x+y);
+    }
     public static void main(String[] args) {
         mymethod();
+        function(10, 20);
     }
     
 }
